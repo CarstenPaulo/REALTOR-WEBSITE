1dcd20c6a2128b953e57d4c43f68c1c2526f8332
CarstenPaulo <paulocarstenk7@gmail.com>
2021-09-24 19:43:42 -0300

    updated

--- project/settings.py
+++ project/settings.py
@@ -140,7 +140,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'corvobrancoimoveis@gmail.com'
-EMAIL_HOST_PASSWORD = 'corvobranco2021'
+EMAIL_HOST_PASSWORD = 'admincorvobranco2021'
 EMAIL_PORT = 587
 
 
