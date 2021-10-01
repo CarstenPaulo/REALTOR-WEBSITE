25cfd2c3f60b8a1eb34a717c67b1bcbdb1963fd4
CarstenPaulo <paulocarstenk7@gmail.com>
2021-09-30 23:14:14 -0300

    a

--- project/settings.py
+++ project/settings.py
@@ -47,8 +47,6 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
 
 ROOT_URLCONF = 'project.urls'
 
