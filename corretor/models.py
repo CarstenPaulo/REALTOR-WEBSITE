a5973f64fac52c15ea3b233665584c16d126c233
CarstenPaulo <paulocarstenk7@gmail.com>
2021-10-28 15:55:12 -0300

    a

--- corretor/models.py
+++ corretor/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Corretor(models.Model):
     nome = models.CharField(max_length=100)
     foto = models.ImageField(upload_to='photos/%d/%m/%Y')
-    telefone = models.CharField(max_length=50, blank=True)
+    telefone = models.CharField(max_length=50)
     creci = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(max_length=100, blank=True)
     descricao = models.TextField( blank=True)
 
     def __str__(self):
